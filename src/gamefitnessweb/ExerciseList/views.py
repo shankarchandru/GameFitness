1877b3238a4bf3e64067e4d1a79d0b5e0200b851
Andrew <andrew.shults@cgu.edu>
2019-10-14 21:11:55 -0700

    added each sport to the ExerciseList Url

--- src/gamefitnessweb/ExerciseList/views.py
+++ src/gamefitnessweb/ExerciseList/views.py
@@ -13,7 +13,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.order_by('-pub_date')[:10]
 
 
 class DetailView(generic.DetailView):
